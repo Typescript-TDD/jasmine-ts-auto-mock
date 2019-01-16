aaba91c47e692f1148ddcd861aca93f02e369d83
uittorio <vittorio.gue@gmail.com>
2019-01-16 08:26:15 +0000

    update to the latest version of ts-auto-mock

---
+++ config/webpack.build.js
@@ -0,0 +1,26 @@
+/*globals module, require */
+const path = require('path');
+
+module.exports = {
+    mode: "production",
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'awesome-typescript-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    entry: {
+        index: ['./index.ts'],
+    },
+    output: {
+        libraryTarget: "commonjs",
+        path: path.resolve(__dirname, ".."),
+        filename: "[name].js"
+    }
+};
