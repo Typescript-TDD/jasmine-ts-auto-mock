854148c5fdf461a61043af73c1a1e38aedd0e5c8
Vittorio Guerriero <vittorio.gue@gmail.com>
2019-03-10 09:53:14 +0000

    update ts-auto-mock to the latest version

--- examples/karma-webpack/test/name.test.ts
+++ examples/karma-webpack/test/name.test.ts
@@ -2,15 +2,15 @@ import { createMock, mockedMethod, On } from "ts-auto-mock";
 
 describe('names', () => {
     it('should give a name to the spy', () => {
-        interface Interface {
-            a(): string;
+    interface Interface {
+        a(): string;
             b: () => string;
-        }
-
+    }
+    
         const mock: Interface = createMock<Interface>();
         const aSpy = On.Mock(mock).get(mockedMethod(x => x.a));
         const bSpy = On.Mock(mock).get(mockedMethod(x => x.b));
-
+        
         expect(aSpy.and.identity).toBe("a");
         expect(bSpy.and.identity).toBe("b");
     });
